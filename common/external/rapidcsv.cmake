8e61e9ae02f96cbee698a86cc6c36bfcbf8dc74b
Pavel Siska <siska@cesnet.cz>
2024-01-09 13:16:36 +0100

    CMake - introduce rapidcsv library

---
+++ common/external/rapidcsv.cmake
@@ -0,0 +1,9 @@
+# rapidcsv library (modern C++ csv parser)
+
+FetchContent_Declare(
+  rapidcsv
+  GIT_REPOSITORY "https://github.com/d99kris/rapidcsv.git"
+  GIT_TAG        "v8.80"
+)
+
+FetchContent_MakeAvailable(rapidcsv)
