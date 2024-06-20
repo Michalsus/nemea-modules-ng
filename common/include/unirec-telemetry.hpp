4346eab76513e5019b38c036ccd3c517398b052f
Pavel Siska <siska@cesnet.cz>
2024-06-20 02:03:41 +0200

    unirec-telemetry - add unirec-telemetry to the common object library This commit introduces a unirec-telemetry (part of common library) which provides functions to retrieve telemetry data from Unirec interfaces.
    The telemetry data includes:
    - receivedBytes
    - receivedRecords
    - missedRecords
    - missed percentage

---
+++ common/include/unirec-telemetry.hpp
@@ -0,0 +1,47 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @brief Telemetry functions for Unirec interfaces.
+ *
+ * This file contains functions to retrieve telemetry data from Unirec interfaces.
+ *
+ * Example output format:
+ *
+ * @code
+ * receivedBytes = XXX
+ * receivedRecords = XXX
+ * missedRecords = XXX
+ * missed = XX %
+ * @endcode
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#include <telemetry.hpp>
+#include <unirec++/unirec.hpp>
+
+namespace nm {
+
+/**
+ * @brief Retrieves telemetry data for an input Unirec interface.
+ *
+ * This function retrieves and returns telemetry data for a given input Unirec interface.
+ *
+ * @param interface The input Unirec interface.
+ * @return telemetry::Content The telemetry data of the interface.
+ */
+telemetry::Content getInterfaceTelemetry(const Nemea::UnirecInputInterface& interface);
+
+/**
+ * @brief Retrieves telemetry data for a bidirectional Unirec interface.
+ *
+ * This function retrieves and returns telemetry data for a given bidirectional Unirec interface.
+ *
+ * @param interface The bidirectional Unirec interface.
+ * @return telemetry::Content The telemetry data of the interface.
+ */
+telemetry::Content getInterfaceTelemetry(const Nemea::UnirecBidirectionalInterface& interface);
+
+} // namespace nm
---
+++ common/src/unirec-telemetry.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @brief
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "unirec-telemetry.hpp"
+
+namespace nm {
+
+static double getMissedPercentage(const Nemea::InputInteraceStats& stats)
+{
+	if (!stats.receivedRecords && !stats.missedRecords) {
+		return 0;
+	}
+
+	double fraction = static_cast<double>(stats.missedRecords)
+		/ static_cast<double>(stats.receivedRecords + stats.missedRecords);
+
+	const int fractionToPercentage = 100;
+	return fraction * fractionToPercentage;
+}
+
+static telemetry::Content createInterfaceTelemetry(const Nemea::InputInteraceStats& stats)
+{
+	telemetry::Dict dict;
+	dict["receivedBytes"] = stats.receivedBytes;
+	dict["receivedRecords"] = stats.receivedRecords;
+	dict["missedRecords"] = stats.missedRecords;
+	dict["missed"] = telemetry::ScalarWithUnit(getMissedPercentage(stats), "%");
+	return dict;
+}
+
+telemetry::Content getInterfaceTelemetry(const Nemea::UnirecBidirectionalInterface& interface)
+{
+	const auto stats = interface.getInputInterfaceStats();
+	return createInterfaceTelemetry(stats);
+}
+
+telemetry::Content getInterfaceTelemetry(const Nemea::UnirecInputInterface& interface)
+{
+	const auto stats = interface.getInputInterfaceStats();
+	return createInterfaceTelemetry(stats);
+}
+
+} // namespace nm
